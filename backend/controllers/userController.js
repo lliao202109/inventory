038a2c7032fef6a027aa6b2b9c60e23f69d5d9aa
linaliao <nanachanliao@gmail.com>
2024-09-23 20:29:29 -0700

    add user logout route and controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -103,10 +103,19 @@ if(user && passwordIsCorrect){
 })
 
 
-
+const logoutUser = asyncHandler(async(req,res) =>{
+   res.cookie('token','',{
+    path: "/",
+    httpOnly: true,
+    expires: new Date(0),
+    sameSite: "none",
+    secure: true,   })
+    return res.status(200).json({message:'Successfully logged out!'})
+})
 
 
 module.exports ={
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 }
