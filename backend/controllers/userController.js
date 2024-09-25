8a54ef5c127b754bbb3ddbe8860657f9d85f0e30
linaliao <nanachanliao@gmail.com>
2024-09-24 22:51:18 -0700

    complete getUser and protect

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -113,9 +113,23 @@ const logoutUser = asyncHandler(async(req,res) =>{
     return res.status(200).json({message:'Successfully logged out!'})
 })
 
+const getUser = asyncHandler(async(req, res) =>{
+    const user = await User.findById(req.user._id)
+    if(user){
+        const {_id, name, email, photo, phone, bio} = user
+          res.status(200).json({
+              _id, name, email, photo, phone, bio
+          })
+      }else{
+          res.status(400)
+          throw new Error ("User not found")
+  
+      }
+})
 
 module.exports ={
     registerUser,
     loginUser,
-    logoutUser
+    logoutUser,
+    getUser
 }
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const {registerUser, loginUser, logoutUser}= require('../controllers/userController')
+const {registerUser, loginUser, logoutUser, getUser}= require('../controllers/userController')
+
+const protect = require('../middleWare/authMiddleWare')
 
 
 router.post('/register', registerUser)
 router.post('/login', loginUser)
 router.get('/logout',logoutUser)
+router.get('/getuser', protect, getUser)
 
 
 module.exports = router
