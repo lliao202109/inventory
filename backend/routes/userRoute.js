8bb7d388af31df7dc4d11cebf1ec55e464fcfbc5
linaliao <nanachanliao@gmail.com>
2024-09-25 20:29:10 -0700

    update user route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {registerUser, loginUser, logoutUser, getUser, loginStatus}= require('../controllers/userController')
+const {registerUser, loginUser, logoutUser, getUser, loginStatus, updateUser}= require('../controllers/userController')
 
 const protect = require('../middleWare/authMiddleWare')
 
@@ -10,6 +10,8 @@ router.post('/login', loginUser)
 router.get('/logout',logoutUser)
 router.get('/getuser', protect, getUser)
 router.get('/loggedin',loginStatus)
+router.patch('/updateUser',protect,updateUser)
+
 
 
 module.exports = router
