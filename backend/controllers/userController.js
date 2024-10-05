b11b4cbb7af74af45ea5a08145346e69755c4c95
linaliao <nanachanliao@gmail.com>
2024-10-05 14:19:49 -0700

    use nodemail to send email when user forgets pw

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,9 @@ const asyncHandler = require("express-async-handler")
 const User = require('../models/userModel')
 const jwt = require('jsonwebtoken');
 const bcrypt = require("bcryptjs")
+const Token = require('../models/tokenModel')
+const crypto = require('crypto');
+const sendEmail = require("../utils/sendEmail")
 
 
 const generateToken = (id) => {
@@ -192,8 +195,58 @@ const changePassword = asyncHandler(async (req, res) => {
     }
 })
 
-const forgotPassword = asyncHandler(async(req,res) => {
-    res.send('resent')
+const forgotPassword = asyncHandler(async (req, res) => {
+    const { email } = req.body
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        res.status(404)
+        throw new Error('User does not exist')
+    }
+
+    // Delete token if it exists in DB
+    let token = await Token.findOne({ userId: user._id });
+    if (token) {
+        await token.deleteOne();
+    }
+
+    // Create Reset Token 
+    let resetToken = crypto.randomBytes(32).toString("hex") + user._id
+
+    // Hash token before saving to DB
+    const hashedToken = crypto.createHash('sha256').update(resetToken).digest("hex")
+
+    // Save token to DB
+    await new Token({
+        userId: user._id,
+        token: hashedToken,
+        createdAt: Date.now(),
+        expiresAt: Date.now() + 30 * (60 * 1000) // Thirty minutes
+    }).save()
+
+    // Construct reset URL 
+    const resetURL = `${process.env.FRONTEND_URL}/resetpassword/${resetToken}`
+
+    // Reset Email
+    const message = `
+    <h2> Hello ${user.name} </h2>
+    <p> Please use the url below to reset password </p>
+    <p> This reset link is valid for 30 minutes </p>
+    <a href=${resetURL} clicktracking=off>${resetURL}</a>
+    <p>Regards...</p>
+    `
+    const subject = "Password Reset Request";
+    const send_to = user.email;
+    const sent_from = process.env.EMAIL_USER;
+    console.log('message: ', message)
+    try {
+        await sendEmail(subject, message, send_to, sent_from);
+        res.status(200).json({ success: true, message: "Reset Email Sent" });
+    } catch (error) {
+        res.status(500);
+        console.log('****', error)
+        throw new Error("Email not sent, please try again");
+    }
 })
 
 module.exports = {
--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,14 +3,10 @@ const nodemailer = require('nodemailer')
 const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
     // Create email transporter
     const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: 587,
+        service: 'gmail',
         auth: {
             user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        },
-        tls: {
-            rejectUnauthorized: false
+            pass: process.env.EMAIL_PASS
         }
     })
 
@@ -25,10 +21,11 @@ const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
 
     // send email
     transporter.sendMail(options, function (error, info) {
-        if (err) {
-            console.log(err)
+        if (error) {
+            console.log(error)
+        }else{
+            console.log(info)
         }
-        console.log(info)
     })
 
 
